4b475ee7f393bde1bf0491b60ee0c7d67b67fa9b
amogh <114508501+AmoghKar1@users.noreply.github.com>
2023-03-28 22:25:34 +0530

    Update messages.py

--- tools/stm32w/stm32w_flasher/py_files/messages.py
+++ tools/stm32w/stm32w_flasher/py_files/messages.py
@@ -3,6 +3,7 @@
 # Extraction and little adaptation performed by E.Duble (CNRS, LIG).
 
 import sys
+import time
 
 
 def errorMessage(msg, header=True):
@@ -10,16 +11,17 @@ def errorMessage(msg, header=True):
 		sys.stderr.write('ERROR: ')
 	sys.stderr.write(msg)
 	sys.stderr.flush()
-
+    time(1)
 def infoMessage(msg, header=True):
 	if header:
 		sys.stdout.write('INFO: ')
 	sys.stdout.write(msg)
 	sys.stdout.flush()
-
+        time(1)
 def warningMessage(msg, header=True):
 	if header:
 		sys.stderr.write('WARNING: ')
 	sys.stderr.write(msg)
 	sys.stderr.flush()
+	time(1)
 
